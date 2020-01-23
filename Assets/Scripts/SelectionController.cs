0a6dbb6531a67053bad0d2f9ce944551ab3e9588
Alex Sampson <32361325+15015483Alex@users.noreply.github.com>
2020-01-23 11:10:19 +0000

    Basic Ray Selection

---
+++ Assets/Scripts/SelectionController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(RaySelection))]
+
+public class SelectionController : MonoBehaviour
+{
+    public GameObject objectSelected;
+    public RaySelection rs;
+    public Camera cam;
+    public LayerMask objectLayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = Camera.main;
+        rs = GetComponent<RaySelection>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetAxisRaw("Select") == 1f)
+        {
+            GameObject os = rs.Select(cam.ScreenPointToRay(Input.mousePosition), objectLayer);
+            objectSelected = os;
+        }
+    }
+}
