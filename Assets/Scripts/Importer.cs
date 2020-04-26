61556781cb3d2b7cffe2f01506f84ff14ada891f
Alex Sampson <32361325+15015483Alex@users.noreply.github.com>
2020-04-26 21:47:06 +0100

    Import Bug Fix

--- Assets/Scripts/Importer.cs
+++ Assets/Scripts/Importer.cs
@@ -15,7 +15,6 @@ public class Importer : MonoBehaviour
     void Start()
     {
         mapDir = Application.dataPath + "/Map Files/";
-        ImportMap("test");
     }
 
     // Update is called once per frame
